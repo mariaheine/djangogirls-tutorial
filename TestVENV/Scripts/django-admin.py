0c226e7eebdf331d12331fae092aecc40201a78d
psthmn-cnd <altanhir@gmail.com>
2018-09-26 17:25:21 +0200

    .gitignore updated

--- TestVENV/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\sanctuary\repos\_web\01_djangogirlstutorial\testvenv\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
